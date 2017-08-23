ebeba9e8df428dc5af7d0be49260adcb8c38c2fd
nichharp <nicholas.harper@qa.com>
2017-08-23 13:58:50 +0000

    Got powers to print. Need to get the heroes names to print and format it

--- myExercises.js
+++ myExercises.js
@@ -13,8 +13,9 @@ function makerPerson(x, y, z) {
     Person.name = x;
     Person.age = y;
     Person.occupation = z;
-    document.write(Person.name + " " + Person.age + " " + Person.occupation + " ");
-    console.log(Person.name + " " + Person.age + " " + Person.occupation + " ");
+    document.getElementById("message").innerHTML = Person.name + " " + Person.age + " " + Person.occupation + " ";
+    // document.write();
+    // console.log(Person.name + " " + Person.age + " " + Person.occupation + " ");
 }
 function boop() {
     window.alert("D0n'T PrEsS tHe BUtT00n");
@@ -25,21 +26,22 @@ function increaseAge() {
 }
 function checkAge() {
     if (Person.age <= 40 & Person.age >= 20) {
-        document.write(Person.name + " is between 20-40");
+        document.getElementById("message").innerHTML = Person.name + " is between 20-40";
     }
 }
 function loopOne() {
+    document.getElementById("message").innerHTML = "";
     for (var i = 1; i <= 10; i++) {
-        document.write(i);
-        document.write("<br>");
-
+        document.getElementById("message").innerHTML += i;
+        document.getElementById("message").innerHTML += "<br>";
     }
 }
 function loopTwo() {
+    document.getElementById("message").innerHTML = "";
     for (var j = 1; j <= 200; j++) {
         if (j % 2 === 0) {
-            document.write(j);
-            document.write("<br>");
+            document.getElementById("message").innerHTML += j;
+            document.getElementById("message").innerHTML += "<br>";
         }
     }
 }
@@ -47,34 +49,35 @@ function loopFizzBuzz() {
     var a = document.getElementById("FizzBuzzOne").value;
     var b = document.getElementById("FizzBuzzTwo").value;
     var c = document.getElementById("FizzBuzzThree").value;
+    document.getElementById("message").innerHTML = "";
     for (var i = 1; i <= a; i++) {
         if (i % 3 === 0 & i % 5 === 0) {
-            document.write(b + c);
-            document.write("<br>");
+            document.getElementById("message").innerHTML += b + c;
+            document.getElementById("message").innerHTML += "<br>";
         } else if (i % 3 === 0) {
-            document.write(b);
-            document.write("<br>");
+            document.getElementById("message").innerHTML += b;
+            document.getElementById("message").innerHTML += "<br>";
         } else if (i % 5 === 0) {
-            document.write(c);
-            document.write("<br>");
+            document.getElementById("message").innerHTML += c;
+            document.getElementById("message").innerHTML += "<br>";
         }
     }
 }
 function loopFour(b) {
     var a = parseInt(b);
-    document.write(a);
-    document.write("<br>");
+    document.getElementById("message").innerHTML = a;
+    document.getElementById("message").innerHTML += "<br>";
     while (a !== 1) {
         if (a % 3 === 0) {
-            document.write("divide by 3   -   ");
+            document.getElementById("message").innerHTML += "divide by 3   -   ";
             a = a / 3;
-            document.write(a);
-            document.write("<br>");
+            document.getElementById("message").innerHTML += a;
+            document.getElementById("message").innerHTML += "<br>";
         } else {
-            document.write("Add 1    -   ");
+            document.getElementById("message").innerHTML += "Add 1    -   ";
             a += 1;
-            document.write(a);
-            document.write("<br>");
+            document.getElementById("message").innerHTML += a;
+            document.getElementById("message").innerHTML += "<br>";
 
         }
     }
@@ -86,7 +89,7 @@ function triples(a) {
             count += 1;
         }
     }
-    document.write(count);
+    document.getElementById("message").innerHTML = count;
 }
 function DOMcreate() {
     document.getElementById("message").innerHTML = "<p> A luau (Hawaiian: lūʻau) is a traditional Hawaiian party or feast that is usually accompanied by entertainment. It may feature food such as poi, Kalua pig, poke, lomi salmon, opihi, haupia and beer, and entertainment such as traditional Hawaiian music and hula. Among people from Hawaiʻi, the concepts of sfsfs and sdfsdfs are often blended, resulting in graduation luau, wedding luau and birthday luau. </p>";
@@ -110,7 +113,51 @@ function JsonOne() {
         // var myH1 = document.createElement("h1");
         // myH1.textContent = requestData["squadName"];
         // document.getElementsByTagName("head")[0].appendChild(myH1);
-        console.log(requestData);
+
+        document.getElementById("message").innerHTML = "";
+        //loop through squadname, hometown, formed, scretbase, active
+        // then for members
+        //loop through name, age, secret Identity
+        //then for powers
+        //loop through powers
+        // console.log(requestData.squadName);
+        for (var name in requestData) {
+            if (name === "members") {
+                for (let j = 0; j < requestData.members.length; j++) {
+                    for (var name2 in requestData.members[j]) {
+                        if (name2 === "powers") {
+                            for (let i = 0; i < requestData.members[j].powers.length; i++) {
+                                document.getElementById("message").innerHTML += name2 + ": " + requestData.members[j][name2][i];
+                            }
+                        }
+                        else if (requestData.members.hasOwnProperty(name2)) {
+                            console.log(requestData[name]);
+                            document.getElementById("message").innerHTML += name2 + ": " + requestData.members[j][name2] + ", " + "<br>";
+                        }
+                    }
+                }
+            }
+            else if (requestData.hasOwnProperty(name)) {
+                document.getElementById("message").innerHTML += name + ": " + requestData[name] + ", " + "<br>";
+            }
+        }
+
+
+
+        // for (let i = 0; i < requestData.length - 1; i++) {
+        //     document.getElementById("message").innerHTML += requestData[i] + ", ";
+        // }
+        // console.log(requestData[5].length);
+        // for (let i = 0; i < requestData[5].length; i++) {
+        //     document.getElementById("message").innerHTML += requestData[i].members[j].name + ", " + requestData[i].members[j].age + ", " + requestData[i].members[j].secretIndentity + ", ";
+        //     if (i === 3) {
+        //         document.getElementById("message").innerHTML += "Powers : ";
+        //         for (let j = 0; j < requestData[5].members[i].length; j++) {
+        //             document.getElementById("message").innerHTML += requestData[5].members[i].powers[j];
+        //         }
+        //     }
+        // }
+
         // 	for (var i =0; i<requestData.length; i++) {
         // 		// console.log(requestData[i].nm);
 
@@ -159,18 +206,19 @@ function print(a) {
 function search(a) { //works for single year Kings & works for full input years "#-#". Couldn't make work to find years in string... ie: search for #1, found in "#1-#2"
     //a is my input
     // JsonTwo();
-
+    document.getElementById("message").innerHTML = "";
     for (var i = 0; i < requestData.length; i++) {
         var data = requestData[i];
         var years = data.yrs.toString();
         if (a.indexOf(data.nm) !== -1) {
+            document.getElementById("message").innerHTML += requestData[i].nm + ", " + requestData[i].cty + ", " + requestData[i].hse + ", " + requestData[i].yrs;
             console.log(requestData[i]);
         }
         else if (a.indexOf(data.cty) !== -1) {
-            console.log(requestData[i]);
+            document.getElementById("message").innerHTML += requestData[i].nm + ", " + requestData[i].cty + ", " + requestData[i].hse + ", " + requestData[i].yrs;
         }
         else if (a.indexOf(data.hse) !== -1) {
-            console.log(requestData[i]);
+            document.getElementById("message").innerHTML += requestData[i].nm + ", " + requestData[i].cty + ", " + requestData[i].hse + ", " + requestData[i].yrs;
         }
         // console.log(years);
         // console.log(a);
@@ -178,7 +226,7 @@ function search(a) { //works for single year Kings & works for full input years 
         // console.log(typeof a);
         // console.log(typeof years);
         else if (a.indexOf(years) !== -1) {
-            console.log(requestData[i]);
+            document.getElementById("message").innerHTML += requestData[i].nm + ", " + requestData[i].cty + ", " + requestData[i].hse + ", " + requestData[i].yrs;
         }
     }
 
