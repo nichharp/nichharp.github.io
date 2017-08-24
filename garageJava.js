54e9b54871418c0e835dc1d6e06454bab030d60e
nichharp <nicholas.harper@qa.com>
2017-08-24 15:14:36 +0000

    small errors

--- garageJava.js
+++ garageJava.js
@@ -2,8 +2,6 @@ let CarList = [];
 let num = 0;
 
 
-
-
 function addCar(carName, carMake, carMiles, numFaults) {
     let bool = false;
     Car = { num, carName, carMake, carMiles, numFaults, bool }; //if continuing to use Stringify, need to change these parameters to make it easier to read.
@@ -57,19 +55,60 @@ function removeCar(a) {
         document.getElementById("message").innerHTML += "Your " + CarList[a].carMake + " has not been billed. Please bill first";
     }
 }
+
 function billEveryone() {
-    for (let i = 0; i < CarList[-1].num; i++) {
-        billMeBaby(i);
+    document.getElementById("message").innerHTML = "";
+
+    for (let i = 0; i < num; i++) {
+        billEveryoneBaby(i);
     }
 }
+function billEveryoneBaby(b) {
+    let k = 0;
+    b = parseInt(b);
+    for (let i = 0; i < num; i++) {
+        // console.log(i, CarList[i].num, num, b);
+        if (typeof CarList[i].num === "undefined") {
 
+        }
+        else if (CarList[i].num === b) {
+
+            k++;
+            // console.log(parseInt(CarList[b].carMiles));
+            // console.log(parseInt(CarList[b].num));
+            // console.log(parseInt(CarList[b].numFaults));
+
+            let p = parseInt(CarList[b].carMiles) * 3 + parseInt(CarList[b].num) * 9 + parseInt(CarList[b].numFaults) * 10;
+            if (CarList[b].carMake === "BMW") {
+                p += 99999;
+            }
+            if (CarList[b].carMake === "Audi") {
+                p += 88888;
+            }
+            if (CarList[b].carName === "Elliot") {
+                p += 9999999999999999999999999;
+            }
+            document.getElementById("message").innerHTML += "The Car with the Car Number of " + b + " is to be charged £" + p + ". Please see reception to pay. <br>";
+            CarList[b].bool = true;
+            break;
+        }
+        // else {
+        //     document.getElementById("message").innerHTML += "This Car with the Car Number of " + b + " does not exist.";
+        // }
+    }
+    if (k === 0) {
+        document.getElementById("message").innerHTML += "This Car with the Car Number of " + b + " does not exist. <br>";
+    }
+}
 
 function billMeBaby(b) {
     let k = 0;
     document.getElementById("message").innerHTML = "";
-    for (let i = 0; i < CarList.length; i++) {
-        b = parseInt(b);
-        console.log(i, CarList[i].num, b);
+    b = parseInt(b);
+
+    for (let i = 0; i < num; i++) {
+        console.log(i, CarList[i].num, num, b);
+        console.log(CarList);
         if (CarList[i].num === b) {
 
             k++;
@@ -96,7 +135,7 @@ function billMeBaby(b) {
         // }
     }
     if (k === 0) {
-        document.getElementById("message").innerHTML += Car + ", this Car with the Car Number of " + b + " does not exist.";
+        document.getElementById("message").innerHTML += "This Car with the Car Number of " + b + " does not exist. <br>";
     }
 }
 function notAFun() {
@@ -107,14 +146,24 @@ function notAFun() {
 function commandFun(de) {
     de = de.toLowerCase();
     let com = de.split(" ");
+    // console.log(com.length);
+    // for (let kkk = 0; kkk < com.length; kkk++) {
+    //     console.log(typeof com[kkk]);
+    // }
+
     if (com[0] === "create") {
 
         if (com[1] === "car") {
-            let n = com[2];
-            let m = com[3];
-            let mil = come[4];
-            let f = com[5];
-            addCar(n, m, mil, f);
+            if (com.length === 6) {
+                let n = com[2];
+                let m = com[3];
+                let mil = com[4];
+                let f = com[5];
+                addCar(n, m, mil, f);
+            }
+            else {
+                notAFun();
+            }
         }
         else {
             notAFun();
@@ -134,15 +183,45 @@ function commandFun(de) {
         }
     }
     else if (com[0] === "check" && com[1] === "out") {
-        removeCar(com[2]);
+        if (!isNaN(Number(com[2]))) {
+            removeCar(Number(com[2]));
+        }
+        else {
+            notAFun();
+        }
+
+
+
+
+
     }
     else if (com[0] === "bill") {
-        billMeBaby(com[1]);
+        if (com[1] === "all") {
+            billEveryone();
+        }
+        else if (!isNaN(Number(com[1]))) {
+            // console.log(Number(com[1]));
+
+            billMeBaby(com[1]);
+        }
+        else {
+            notAFun();
+        }
+    }
+    else if (com[0] === "print") {
+        if (!isNaN(Number(com[1]))) {
+            printCar(Number(com[1]));
+        }
+        else {
+            notAFun();
+        }
     }
     else {
         notAFun();
     }
 }
+// console.log(Number("5"));
+// console.log(Number("hghg"));
 
 
 addCar("Killer Mike", "Audi", 123, 1);
