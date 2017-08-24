c448d50cd7a70839dd8873736d236b259f86f210
nichharp <nicholas.harper@qa.com>
2017-08-24 09:41:09 +0000

    update for Garage, checkIn, checkOut, printContents

--- garageJava.js
+++ garageJava.js
@@ -1,16 +1,21 @@
 let CarList = [];
+let num = 0;
 
 function addCar(carName, carMake, carMiles) {
-    let num = CarList.length;
-    Car = { num, carMake, carMiles };
+    let bool = false;
+    Car = { num, carMake, carMiles, bool }; //if continuing to use Stringify, need to change these parameters to make it easier to read.
     CarList.push(Car);
     document.getElementById("message").innerHTML = "";
     document.getElementById("message").innerHTML += "Your Car Number is: " + num + ". Please remember this.";
+    num++;
 
 }
 function printGarage() {
     document.getElementById("message").innerHTML = "";
-    document.getElementById("message").innerHTML = JSON.stringify(CarList);
+    for (let i = 0; i < CarList.length; i++) {
+        document.getElementById("message").innerHTML += JSON.stringify(CarList[i]);
+        document.getElementById("message").innerHTML += "<br>";
+    }
 
     //if i had more time i'd print it properly. But this still looks nice
     // for (let i = 0; i < CarList.length; i++) {
@@ -21,10 +26,35 @@ function printGarage() {
     //     }
     // }
 }
-function removeCar(num) {
-    CarList
+function removeCar(a) {
+    document.getElementById("message").innerHTML = "";
+    if (CarList[a].bool === true) {
+        document.getElementById("message").innerHTML += "Your " + CarList[a].carMake + " has now been checked out";
+        // console.log(CarList);
+        CarList.splice(a, 1);
+        // console.log(CarList);
+    }
+    else {
+        document.getElementById("message").innerHTML += "Your " + CarList[a].carMake + " has not been billed. Please bill first";
+    }
+}
+function billEveryone() {
+
 }
 
+
+function billMeBaby(b) {
+    document.getElementById("message").innerHTML = "";
+    for (let i = 0; i < CarList.length; i++) {
+        if (CarList[i].num === b) {
+            let p = CarList[b].carMiles * 3 + CarList[b].num * 9;
+        }
+        else {
+            document.getElementById("message").innerHTML += "This car does not exist.";
+        }
+    }
+
+}
 
 
 function pro() {
